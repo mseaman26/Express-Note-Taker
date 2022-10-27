8c84b63bb2cd3e345e2406e4be2ac5f4da640274
michael seaman <mseaman26@gmail.com>
2022-10-26 18:27:01 -0700

    got the /notes route working

--- server.js
+++ server.js
@@ -10,11 +10,13 @@ const PORT = 3000;
 
 
 // Routes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname+"/Develop/public", "notes.html"))
+})
+
 app.get('*', (req, res) => res.sendFile(path.join(__dirname+"/Develop/public", 'index.html')))
 
-app.get("/", (req, res) => {
-    
-})
+
 
 
 
