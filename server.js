25d61a93c18b3ecee21205a2931b396d4ec0115c
michael seaman <mseaman26@gmail.com>
2022-10-26 18:44:06 -0700

    added first api route

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const db = require("./Develop/db/db.json")
 
 const PORT = 3000;
 
@@ -14,7 +14,12 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname+"/Develop/public", "notes.html"))
 })
 
+app.get("/api/notes", (req, res) => {
+    res.json(db)
+})
+
 app.get('*', (req, res) => res.sendFile(path.join(__dirname+"/Develop/public", 'index.html')))
+
 
 
 
