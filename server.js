a10902ed833b89f7a1c3611818ebfd25d52824d3
michael seaman <mseaman26@gmail.com>
2022-10-27 12:55:13 -0700

    added express.json and express.urlencoded and I can now get a req.body with data

--- server.js
+++ server.js
@@ -3,7 +3,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const db = require("./Develop/db/db.json")
+const db = require("./db/db.json")
+const generateUniqueID = require("generate-unique-id")
+app.use(express.static('./public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = 3000;
 
@@ -11,15 +15,21 @@ const PORT = 3000;
 
 // Routes
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname+"/Develop/public", "notes.html"))
+    res.sendFile(path.join(__dirname+"/public", "notes.html"))
+})
+
+app.post("/api/notes", (req, res) => {
+    console.info(req.body);
+    // res.json(db)
 })
 
 app.get("/api/notes", (req, res) => {
     res.json(db)
 })
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname+"/Develop/public", 'index.html')))
 
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname+"/public", 'index.html')))
 
 
 
