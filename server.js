7251d18fb09943a498d914f8d01f90e86ee99611
michael seaman <mseaman26@gmail.com>
2022-10-27 20:20:06 -0700

    I'm able to add notes to the DB file and add id numbers, but I cant get the list to update when I click save

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const db = require("./db/db.json")
-const generateUniqueID = require("generate-unique-id")
+const fs = require('fs');
+const generateUniqueID = require("generate-unique-id");
+const { fstat } = require('fs');
 app.use(express.static('./public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,8 +21,30 @@ app.get("/notes", (req, res) => {
 })
 
 app.post("/api/notes", (req, res) => {
-    console.info(req.body);
-    // res.json(db)
+    let updatedNotes
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedNotes = JSON.parse(data);
+          req.body.id = generateUniqueID({length: 4})
+          parsedNotes.push(req.body);
+          updatedNotes = parsedNotes
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (err) =>
+              err
+                ? console.error(err)
+                : console.info('Successfully updated Notes!')
+          );
+          console.log(updatedNotes)
+          res.json(req.body)
+          res.sendFile(path.join(__dirname+"/public", "/notes.html"))
+        }
+      })
+      
+   
 })
 
 app.get("/api/notes", (req, res) => {
